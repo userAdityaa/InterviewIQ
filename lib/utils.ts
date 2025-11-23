126c8ca069351e552672257455be6a761565e2e2
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-11-23 21:12:14 +0530

    config: update application configuration and type definitions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { interviewCovers, mappings } from "@/constants";
+import { interviewCovers, mappings } from "@/appConstants";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
